d2d2477d88429fb8a4db4cdd5e05b0a33410e7a1
thungcodejs <ptuanhungg@gmail.com>
2022-11-16 15:36:37 +0700

    fix Onboarding

--- App.js
+++ App.js
@@ -2,25 +2,35 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+//Screens
 import Onboarding from './screens/Onboarding';
 import Loading from './components/Loading'
 import Profile from './screens/Profile'
 import Main from './screens/Main'
 import Landing from './screens/Landing'
 import Login from './screens/Login'
+
+//redux
 import store from './redux/store'
 import { Provider } from 'react-redux'
+
+
+
+
+
 export default function App() {
   const Stack = createNativeStackNavigator();
   return (
-    <Provider store={store}>
-      <NavigationContainer >
+  <Provider store={store}>
+    <StatusBar style={"light"} />
+    <NavigationContainer >
         <Stack.Navigator initialRouteName='LoginReplace' screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Slides" component={Onboarding} />
-          <Stack.Screen name="Loading" component={Loading} />
-          <Stack.Screen name="Profile" component={Profile} />
-          <Stack.Screen name="Main" component={Main} />
           <Stack.Screen name="Landing" component={Landing} />
+          {/*<Stack.Screen name="Loading" component={Loading} />*/}
+          {/*<Stack.Screen name="Profile" component={Profile} />*/}
+          {/*<Stack.Screen name="Main" component={Main} />*/}
           <Stack.Screen name="Login" component={Login} />
         </Stack.Navigator>
       </NavigationContainer>
--- slides.js
+++ slides.js
@@ -3,30 +3,30 @@ export default [
     id: '1',
     title: 'Quick & Easy Payments',
     description: 'Grow your business by accepting card payments with the new card reader',
-    image: require('./assets/images/5870151cee14b617038b7150..png')
+    image: require('./assets/images/slider1.jpeg')
   },
   {
     id: '2',
     title: 'Quick & Easy Payments',
     description: 'Grow your business by accepting card payments with the new card reader',
-    image: require('./assets/images/5870151cee14b617038b7150..png')
+    image: require('./assets/images/slider4.jpeg')
   },
   {
     id: '3',
     title: 'Quick & Easy Payments',
     description: 'Grow your business by accepting card payments with the new card reader',
-    image: require('./assets/images/5870151cee14b617038b7150..png')
+    image: require('./assets/images/slider3.jpeg')
   },
   {
     id: '4',
     title: 'Quick & Easy Payments',
     description: 'Grow your business by accepting card payments with the new card reader',
-    image: require('./assets/images/5870151cee14b617038b7150..png')
+    image: require('./assets/images/slider5.jpeg')
   },
-  {
-    id: '5',
-    title: 'Quick & Easy Payments',
-    description: 'Grow your business by accepting card payments with the new card reader',
-    image: require('./assets/images/5870151cee14b617038b7150..png')
-  }
+  // {
+  //   id: '5',
+  //   title: 'Quick & Easy Payments',
+  //   description: 'Grow your business by accepting card payments with the new card reader',
+  //   image: require('./assets/images/5870151cee14b617038b7150..png')
+  // }
 ]
