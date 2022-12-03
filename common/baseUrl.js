74cc2d387da5b46720d4cb6c08ca0c9695dd251e
Huan Nguyen <huan06949@gmail.com>
2022-12-03 16:49:06 +0700

    change baseUrl

--- common/baseUrl.js
+++ common/baseUrl.js
@@ -5,7 +5,7 @@ let baseUrl = "";
 {
 
     Platform.OS == 'android'
-        ? baseUrl = 'https://624c-58-186-90-86.ap.ngrok.io/api/v1/'
+        ? baseUrl = 'https://dde4-58-186-90-86.ap.ngrok.io/api/v1/'
         : baseUrl = 'https://624c-58-186-90-86.ap.ngrok.io/api/v1/'
 }
 export default baseUrl;
